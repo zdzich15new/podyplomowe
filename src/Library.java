3c9725d06c91595563cd9d71d138c5c79eb37206
styszko <danielstyszko@gmail.com>
2022-10-09 14:23:37 +0200

    adding_library_class

---
+++ src/Library.java
@@ -0,0 +1,12 @@
+public class Library {
+
+    public static void main(String[] args) {
+        String bookName = "Java przewodnik dla początkujacych";
+        int year = 2021;
+        String author = "Herbert Schildt";
+
+        System.out.println("Book name: " + bookName);
+        System.out.println("Author : " + author);
+        System.out.println("Year: " + year );
+    }
+}
